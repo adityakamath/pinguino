43ae2499249eaf9682a32e2ba21dde09815e2434
Aditya Kamath <adityakamath@live.com>
2020-09-05 00:57:07 +0200

    Update pinguino.ino

--- pinguino.ino
+++ pinguino.ino
@@ -1,5 +1,3 @@
-#include "arduino_secrets.h"
-#include <WiFiNINA.h>
 #include <ArduinoHttpClient.h>
 #include <BlynkSimpleWiFiNINA.h>
 
@@ -10,6 +8,7 @@
 
 // Define NeoPixel pin and initialize object
 #define PIN 6
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
 
 const char ssid[] = SECRET_SSID;    // Network SSID (name)
@@ -55,6 +54,131 @@ int status = WL_IDLE_STATUS;
 WiFiSSLClient client;
 HttpClient http_client = HttpClient(client, server, port);
 
+//NEOFIRE STUFF -----------https://github.com/RoboUlbricht/arduinoslovakia/blob/master/neopixel/neopixel_fire01/neopixel_fire01.ino----------------------------------
+uint32_t fire_color   = strip.Color ( 80,  35,  00);
+uint32_t off_color    = strip.Color (  0,  0,  0);
+
+///
+/// Fire simulator
+///
+class NeoFire
+{
+  Adafruit_NeoPixel &strip;
+ public:
+
+  NeoFire(Adafruit_NeoPixel&);
+  void Draw();
+  void Clear();
+  void AddColor(uint8_t position, uint32_t color);
+  void SubstractColor(uint8_t position, uint32_t color);
+  uint32_t Blend(uint32_t color1, uint32_t color2);
+  uint32_t Substract(uint32_t color1, uint32_t color2);
+};
+
+///
+/// Constructor
+///
+NeoFire::NeoFire(Adafruit_NeoPixel& n_strip)
+: strip (n_strip)
+{
+}
+
+///
+/// Set all colors
+///
+void NeoFire::Draw()
+{
+Clear();
+
+for(int i=0;i<24;i++)
+  {
+  AddColor(i, fire_color);
+  int r = random(80);
+  uint32_t diff_color = strip.Color ( r, r/2, r/2);
+  SubstractColor(i, diff_color);
+  }
+  
+strip.show();
+}
+
+///
+/// Set color of LED
+///
+void NeoFire::AddColor(uint8_t position, uint32_t color)
+{
+uint32_t blended_color = Blend(strip.getPixelColor(position), color);
+strip.setPixelColor(position, blended_color);
+}
+
+///
+/// Set color of LED
+///
+void NeoFire::SubstractColor(uint8_t position, uint32_t color)
+{
+uint32_t blended_color = Substract(strip.getPixelColor(position), color);
+strip.setPixelColor(position, blended_color);
+}
+
+///
+/// Color blending
+///
+uint32_t NeoFire::Blend(uint32_t color1, uint32_t color2)
+{
+uint8_t r1,g1,b1;
+uint8_t r2,g2,b2;
+uint8_t r3,g3,b3;
+
+r1 = (uint8_t)(color1 >> 16),
+g1 = (uint8_t)(color1 >>  8),
+b1 = (uint8_t)(color1 >>  0);
+
+r2 = (uint8_t)(color2 >> 16),
+g2 = (uint8_t)(color2 >>  8),
+b2 = (uint8_t)(color2 >>  0);
+
+return strip.Color(constrain(r1+r2, 0, 255), constrain(g1+g2, 0, 255), constrain(b1+b2, 0, 255));
+}
+
+///
+/// Color blending
+///
+uint32_t NeoFire::Substract(uint32_t color1, uint32_t color2)
+{
+uint8_t r1,g1,b1;
+uint8_t r2,g2,b2;
+uint8_t r3,g3,b3;
+int16_t r,g,b;
+
+r1 = (uint8_t)(color1 >> 16),
+g1 = (uint8_t)(color1 >>  8),
+b1 = (uint8_t)(color1 >>  0);
+
+r2 = (uint8_t)(color2 >> 16),
+g2 = (uint8_t)(color2 >>  8),
+b2 = (uint8_t)(color2 >>  0);
+
+r=(int16_t)r1-(int16_t)r2;
+g=(int16_t)g1-(int16_t)g2;
+b=(int16_t)b1-(int16_t)b2;
+if(r<0) r=0;
+if(g<0) g=0;
+if(b<0) b=0;
+
+return strip.Color(r, g, b);
+}
+
+///
+/// Every LED to black
+///
+void NeoFire::Clear()
+{
+for(uint16_t i=0; i<strip.numPixels (); i++)
+  strip.setPixelColor(i, off_color);
+}
+
+NeoFire fire(strip);
+//END OF NEOFIRE STUFF --------------------------------------------------------------------
+
 void setup() {
   strip.begin();
   strip.setBrightness(slider);
@@ -74,7 +198,7 @@ void loop() {
   int temp_discord  = discord_onoff;
   
   Blynk.run();
-
+    
   if (temp_onoff == 1){
     if(prev_onoff != temp_onoff || hasRun == 0){ discord_send("Pinguino On");}
     hasRun = 1;
@@ -84,7 +208,10 @@ void loop() {
     switch (temp_lampface) {
       case 1: // clock
         if(prev_lampface != temp_lampface){ discord_send("Lampface: Clock");}
+        //Serial.println();
         colorWipe(strip.Color(255, 0, 0), 1);
+        delay(1000);
+        colorWipe(strip.Color(0, 0, 0), 1);
         prev_lampface = 1;
         break;
       
@@ -118,7 +245,7 @@ void loop() {
         
       case 5: //fire
         if(prev_lampface != temp_lampface){ discord_send("Lampface: Fire");}
-        //fire
+        fireface(50);
         prev_lampface = 5;
         break;
         
@@ -182,6 +309,12 @@ void colorWipe(uint32_t c, uint8_t wait) {
     strip.show();
     delay(wait);
   }
+}
+
+// Fill all dots with same color, change colors in a loop
+void fireface(uint8_t wait) {
+  fire.Draw();
+  delay(wait);
 }
 
 // Fill all dots with same color, change colors in a loop
@@ -263,6 +396,6 @@ uint32_t Wheel(byte WheelPos) {
 
 void discord_send(String content) {
   http_client.post(discord_webhook, "application/json", "{\"content\":\"" + content + "\", \"tts\":" + false + "}");
-  Serial.println("[HTTP] Sending message: " + content);
+  //Serial.println("[HTTP] Sending message: " + content);
   
 }
